5605fd2a4fc27860306fdd9b54758370b2dc6cd8
Andre Pangestu <andre.pangestu22@gmail.com>
2021-05-28 08:58:23 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import { Helmet } from 'react-helmet';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import store from './store';
+import { ThemeProvider } from 'styled-components';
+import theme from './utils/theme';
+import GlobalStyle from './utils/globals';
+import AppContainer from './containers/App';
+
+import '../node_modules/react-modal-video/scss/modal-video.scss';
+import '../node_modules/slick-carousel/slick/slick.css';
+import '../node_modules/slick-carousel/slick/slick-theme.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Fragment>
+          <Helmet>
+            <title>Movie Library</title>
+            <meta
+              name="description"
+              content="A Movie Library where you can check all your favorite movies, as well as the cast of it, and so mucnh more! Made with ❤️ by Fidalgo"
+            />
+            <link rel="canonical" href="https://movies.fidalgo.dev" />
+          </Helmet>
+          <AppContainer />
+          <GlobalStyle />
+        </Fragment>
+      </ThemeProvider>
+    </Provider>
   );
 }
 
